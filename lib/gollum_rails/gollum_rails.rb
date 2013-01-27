acee9445a138beedec9d4a4f83985f8787de2717
nirnanaaa <nirnanaaa@khnetworks.com>
2013-01-27 19:15:06 +0100

    test only with 1.9.3

--- lib/gollum_rails/gollum_rails.rb
+++ lib/gollum_rails/gollum_rails.rb
@@ -3,4 +3,6 @@ require 'gollum_rails/config'
 require "gollum_rails/dependency_injector"
 require "gollum_rails/wiki"
 require "gollum_rails/page"
+require 'gollum_rails/file'
+require 'gollum_rails/versions'
 require 'gollum_rails/validations'
