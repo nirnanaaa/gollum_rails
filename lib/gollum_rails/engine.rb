a7251827666bdc5e7f05b382534dc4b8e68f701c
nirnanaaa <nirnanaaa@khnetworks.com>
2013-01-28 19:35:05 +0100

    forgot to put this out

--- lib/gollum_rails/engine.rb
+++ lib/gollum_rails/engine.rb
@@ -3,7 +3,6 @@ require 'rails'
 module GollumRails
   class Engine < ::Rails::Engine
     initializer "gollum_rails.load_app" do |app|
-      puts "abl"
       #super
       ##   GollumRails::Config::rails = app
     end
