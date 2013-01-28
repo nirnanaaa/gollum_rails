8edceb0dc6811d13fd40cd822e42781f95599be8
nirnanaaa <nirnanaaa@khnetworks.com>
2013-01-28 19:46:27 +0100

    class writer test

--- lib/generators/gollum_rails/model/model_generator.rb
+++ lib/generators/gollum_rails/model/model_generator.rb
@@ -5,6 +5,20 @@ module GollumRails
     class ModelGenerator < ::Rails::Generators::Base
       desc "Install a new model into app/models"
       argument :model_name, :type => :string
+      
+      def self.source_root
+        @source_root ||= File.join(File.dirname(__FILE__), 'templates')
+      end
+      
+      def write_class
+        puts class_name
+      end
+      
+      protected
+      
+      def class_name
+        model_name.camelize
+      end
     end
   end
 end
