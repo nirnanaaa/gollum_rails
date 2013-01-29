569aea4bee2ff97e0a5a20a5d2ac5e6d79e55c0a
nirnanaaa <flo@khnetworks.com>
2013-01-29 17:09:53 +0100

    forgot to add page dir

---
+++ lib/gollum_rails/page/actions.rb
@@ -0,0 +1,8 @@
+module GollumRails
+  require ::File.expand_path('../helper', __FILE__)
+  require ::File.expand_path('../find', __FILE__)
+  require ::File.expand_path('../versions', __FILE__)
+  require ::File.expand_path('../delete', __FILE__)
+
+
+end
---
+++ lib/gollum_rails/page/find.rb
@@ -0,0 +1,21 @@
+module GollumRails
+    class Find < PageHelper
+        call_by 'find'
+        attribute :by, :default => nil, :description =>
+
+        def module_run
+
+        end
+        #def module_vars
+        #
+        # =>end
+
+        def module_call
+
+
+        end
+        def mass_assignment(&block)
+
+        end
+    end
+end
