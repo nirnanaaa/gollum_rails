83818edf5f2bef6cfff70816ccbf1e91c367f93a
nirnanaaa <flo@khnetworks.com>
2013-01-29 11:37:57 +0100

    engines ready;)

--- lib/gollum_rails.rb
+++ lib/gollum_rails.rb
@@ -9,10 +9,14 @@ require File.expand_path('../gollum_rails/engine', __FILE__)
 require File.expand_path('../gollum_rails/dependency_injector', __FILE__)
 require File.expand_path('../gollum_rails/config', __FILE__)
 require File.expand_path('../gollum_rails/wiki', __FILE__)
+require File.expand_path('../gollum_rails/validations', __FILE__)
+require File.expand_path('../gollum_rails/file', __FILE__)
+require File.expand_path('../gollum_rails/page', __FILE__)
+require File.expand_path('../gollum_rails/hash', __FILE__)
 
 $KCODE = 'U' if RUBY_VERSION[0,3] == '1.8'
 
 module GollumRails
-  VERSION = '0.0.2.8'
+  VERSION = '0.0.2.9'
 
 end
