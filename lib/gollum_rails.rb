339c672fb6840b9dc2eb49b387a804e392f67dfd
nirnanaaa <flo@khnetworks.com>
2013-01-30 14:48:29 +0100

    what puts in my code?

--- lib/gollum_rails.rb
+++ lib/gollum_rails.rb
@@ -12,11 +12,16 @@ require File.expand_path('../gollum_rails/wiki', __FILE__)
 require File.expand_path('../gollum_rails/validations', __FILE__)
 require File.expand_path('../gollum_rails/file', __FILE__)
 require File.expand_path('../gollum_rails/page', __FILE__)
-require File.expand_path('../gollum_rails/hash', __FILE__)
 
+# gollum still supports ruby 1.8.7 so gollum_rails must also do
+#
 $KCODE = 'U' if RUBY_VERSION[0,3] == '1.8'
 
+# Public: Main application class
+#
 module GollumRails
+
+  # Public: Gets the applications version
   VERSION = '0.0.2.9'
 
 end
