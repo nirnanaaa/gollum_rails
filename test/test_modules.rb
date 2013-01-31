91d734967b055cb0e381705de2d605c594b609ec
nirnanaaa <flo@khnetworks.com>
2013-01-31 07:14:06 +0100

    tests

--- test/test_modules.rb
+++ test/test_modules.rb
@@ -8,7 +8,8 @@ context "Backbone module extender" do
   teardown do
 
   end
-  test "#basic module extender" do
+
+  test "#basic PAGE:: module extender" do
     module GollumRails
       module PageActions
         class Finder < GollumRails::PageHelper
@@ -25,16 +26,31 @@ context "Backbone module extender" do
     gollum_rails = GollumRails::Page.new
     assert_instance_of Array, GollumRails::Page.finder(1,2,3,4)
     assert_instance_of Array, gollum_rails.finder(1,2,3,4)
-    
+
     assert_equal [1,2,3,4], gollum_rails.finder(1,2,3,4)
-    
+
     assert_equal false, GollumRails::DependencyInjector.error
-    
-    
-  end
-  test "#generators color code" do
+
 
   end
+
+  test "#error cleaning / getting " do
+    text = "something happened"
+    GollumRails::DependencyInjector.set({:error => text})
+
+    assert_equal text, GollumRails::DependencyInjector.error
+    assert_instance_of String, GollumRails::DependencyInjector.error
+    assert_equal false, GollumRails::DependencyInjector.error_old
+
+    # clear errors
+    GollumRails::PageHelper.initialized_first
+    assert_instance_of FalseClass, GollumRails::DependencyInjector.error
+    assert_equal false, GollumRails::DependencyInjector.error
+    assert_instance_of String, GollumRails::DependencyInjector.error_old
+    assert_equal text, GollumRails::DependencyInjector.error_old
+
+  end
+
   test "#errors" do
     assert_raise GollumRails::CommitMustBeGivenError do
       raise GollumRails::CommitMustBeGivenError
@@ -46,5 +62,5 @@ context "Backbone module extender" do
       raise GollumRails::ModuleInternalError
     end
   end
-  
+
 end
