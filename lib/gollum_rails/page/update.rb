af100e1fe071f11aff7b970bde6ae2c68fa7bc64
nirnanaaa <nirnanaaa@khnetworks.com>
2013-02-04 19:01:21 +0100

    added a bit to update

--- lib/gollum_rails/page/update.rb
+++ lib/gollum_rails/page/update.rb
@@ -3,18 +3,55 @@ module GollumRails
   module PageActions
     # Public: update actions
     class Update < GollumRails::PageHelper
-        call_by 'update'
-
+      call_by 'update'
+      class << self
         # function initializer
         def initialized_first
-          
-        end
-        
-        # function main call
-        def single_run
-
+          raise GollumRails::PageActions::NoPageLoadedError, "no page loaded! Please verify that you call find() before update()" if
+          not DependencyInjector.page
         end
 
+        # Public: function main call
+        #
+        # name - String of the pages' name (optional on content change)
+        # commit - Hash of commit data (necessary)
+        # content - pages content (updated?) (optional on rename)
+        # format - String of the pages format (optional)
+        # page - Gollum::Page instance (optional)
+        #
+        # Returns the commit string
+        def single_run(*args)
+          name = self.class.f_name(args[0])
+          commit = args[1] if args[1].is_a? ::Hash
+          content = args[2] if not args[2].empty?
+          format = args[3] if not args[3].is_nil?
+          page = args[4] if args[4].is_a? Gollum::Page
+
+          if commit.nil? || content.nil?
+            @error = @options.messages.commit_not_empty_and_content_not_empty
+            return false
+          end
+          commit = DependencyInjector.wiki.update_page(@page, @name, @format, content, commit)
+          if commit.is_a?(String)
+            @persisted = true
+            return commit
+          else
+            @persisted = false
+            return nil
+          end
+        end
+
+      end
+      private
+      def f_name(name)
+        name || GollumRails::DependencyInjector.page.name
+      end
+      def f_content(content)
+      end
+      def f_format(format)
+      end
+      def f_page(page)
+      end
     end
   end
 end
