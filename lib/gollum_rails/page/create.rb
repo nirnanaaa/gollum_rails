3a2863cb3525b208e7ac40451b0a05d2a66410c2
nirnanaaa <nirnanaaa@khnetworks.com>
2013-02-09 11:01:28 +0100

    added create method / class / module

---
+++ lib/gollum_rails/page/create.rb
@@ -0,0 +1,34 @@
+module GollumRails
+  module PageActions
+    # Public: .create or .create! builtin extension for GollumRails::Page
+    class Create < GollumRails::PageHelper
+      # Public: Page.<call_by> . The method call will be generated with this information
+      call_by 'create'
+      call_by 'create!'
+
+
+      class << self
+        # Public: runs the initializer from superclass ( super clears the error buffer )
+        #
+        # Returns nothing
+        def initialized_first
+          super
+        end
+
+        # Public: performs upper methods in Page class
+        #
+        #
+        # Examples
+        #
+        # Returns String
+        def single_run(*argv)
+
+        end
+
+        def mass_assignment(&block)
+
+        end
+      end
+    end
+  end
+end
