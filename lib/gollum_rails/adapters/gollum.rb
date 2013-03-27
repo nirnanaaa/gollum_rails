4b1fd3cdb5e794f117bbbf74d558b26b71408987
Florian Kasper <florian.kasper@corscience.de>
2013-03-27 15:29:11 +0100

    activemodel wooo?

---
+++ lib/gollum_rails/adapters/gollum.rb
@@ -0,0 +1,44 @@
+# ~*~ encoding: utf-8 ~*~
+module GollumRails
+  module Gollum
+
+    # Sets the page class used by all instances
+    attr_writer :page_class
+
+    # Sets the wiki class used by all instances
+    attr_writer :wiki_class
+
+    # Sets the committer
+    attr_writer :committer
+
+    # Gets the Globally used Page class or use a new one if not defined
+    #
+    #
+    # Returns the internal page class or a fresh ::Gollum::Page
+    def page_class
+      @page_class || ::Gollum::Page
+    end
+
+    # Gets the Globally used Page class or use a new one if not defined
+    #
+    #
+    # Returns the internal page class or a fresh ::Gollum::Page
+    def wiki_class
+      @wiki_class || ::Gollum::Wiki
+    end
+
+    # Gets the current committer or using anon
+    #
+    #
+    def committer
+      @committer ||=
+        if Committer.kind_of? ::Gollum::Committer
+          Committer
+        else
+          ::Gollum::Committer
+        end
+    end
+
+  end
+end
+
