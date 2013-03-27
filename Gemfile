ef35b0c84c3dca1603be273652402424ef8ddb82
Florian Kasper <florian.kasper@corscience.de>
2013-03-27 14:06:10 +0100

    new structure

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 
+gem 'gollum-lib'
 gemspec
--- lib/gollum_rails/page/actions.rb
+++
@@ -1,6 +0,0 @@
-module GollumRails
-  require ::File.expand_path('../helper', __FILE__)
-  ::Dir.glob(::File.expand_path(::File.dirname(__FILE__)) + '/*.rb') do |file|
-    require file if file != __FILE__ and !file.include?("helper")
-  end
-end
