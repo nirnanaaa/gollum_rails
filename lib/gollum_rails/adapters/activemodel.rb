03616f3ead4544f3c15ab7c78963b5f1db70650f
Florian Kasper <florian.kasper@corscience.de>
2013-03-28 09:53:06 +0100

    added active model validation with tests

--- lib/gollum_rails/adapters/activemodel.rb
+++ lib/gollum_rails/adapters/activemodel.rb
@@ -1,4 +1,5 @@
 require 'active_model'
+require File.expand_path '../activemodel/boolean', __FILE__
 require File.expand_path '../activemodel/callback', __FILE__
 require File.expand_path '../activemodel/naming', __FILE__
 require File.expand_path '../activemodel/error', __FILE__
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,17 @@ require 'bundler/setup'
 
 require 'gollum_rails'
 
+require 'benchmark'
+RSpec::Matchers.define :take_less_than do |n|
+  chain :seconds do; end
+    match do |block|
+      @elapsed = Benchmark.realtime do
+        block.call
+      end
+      @elapsed <= n
+    end
+end
+
 RSpec.configure do |config|
  config.treat_symbols_as_metadata_keys_with_true_values = true
  config.mock_with :rr
