7aaeedde384fde6c55b3cc53f65e9e3e0af66d3a
nirnanaaa <mosny@zyg.li>
2013-03-28 19:51:36 +0100

    added gollum connector

--- spec/gollum_rails/adapters/gollum/connector_spec.rb
+++ spec/gollum_rails/adapters/gollum/connector_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
-describe GollumRails::Adapters::Gollum do
+describe GollumRails::Adapters::Gollum::Connector do
   before(:each) do
+    @class = GollumRails::Adapters::Gollum::Connector 
   end
-  it "should return the right version" do
-    #GollumRails::Adapters::Gollum.constants.include?("VERSION").should be_true
-    #puts GollumRails::Adapters::Gollum.VERSION
 
+  it "should test the Wiki class connector" do
+    @class.wiki_class.should == GollumRails::Adapters::Gollum::Wiki
   end
+  it "should test the Page class connector" do
+    @class.page_class.should == GollumRails::Adapters::Gollum::Page
+  end
+  it "should test the Committer class connector" do
+    @class.committer_class.should == GollumRails::Adapters::Gollum::Committer
+  end
+    
 end
