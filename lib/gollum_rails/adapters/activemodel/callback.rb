4b2022c9d9aa3fd52b426dbe99f9c6e041be96c8
nirnanaaa <mosny@zyg.li>
2013-03-28 13:19:39 +0100

    callback  content

--- lib/gollum_rails/adapters/activemodel/callback.rb
+++ lib/gollum_rails/adapters/activemodel/callback.rb
@@ -3,17 +3,59 @@ require 'active_model'
 module GollumRails
   module Adapters
     module ActiveModel
+      
+      # Callback handling class
       class Callback
         extend ::ActiveModel::Callbacks
         
-        # Gets/Sets the Error
-        attr_accessor :errors
+        define_model_callbacks :create, :only => [:after, :before]
+        define_model_callbacks :update, :only => [:after, :before]
+        define_model_callbacks :save, :only => [:after, :before]
+        define_model_callbacks :delete, :only => [:after, :before]
+        define_model_callbacks :find, :only => [:after, :before]
 
-        # Initializes a new Instance of ActiveModel
-        # 
-        def initialize
-          @errors = ActiveModel::Errors.new(self)
-        end
+        before_create :before_create
+        before_update :before_update
+        before_save :before_save
+        before_delete :before_delete
+        before_find :before_find
+
+        after_create :after_create
+        after_update :after_update
+        after_save :after_save
+        after_delete :after_delete
+        after_find :after_find
+
+        # Before create action
+        def before_create; end
+        
+        # Before update action
+        def before_update; end
+
+        # Before save action
+        def before_save; end
+
+        # Before delete action
+        def before_delete; end
+
+        # Before find action
+        def before_find; end
+
+        # After create action
+        def after_create; end
+
+        # After update action
+        def after_update; end
+
+        # After save action
+        def after_save; end
+
+        # After delete action
+        def after_delete; end
+
+        # After find action
+        def after_find; end
+
       end
 
     end
