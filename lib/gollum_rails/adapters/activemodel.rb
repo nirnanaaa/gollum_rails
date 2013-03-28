6967eb579d5c55f521ca1caf926efb5fbd9ae981
Florian Kasper <florian.kasper@corscience.de>
2013-03-28 15:03:45 +0100

    documentation updated

--- lib/gollum_rails/adapters/activemodel.rb
+++ lib/gollum_rails/adapters/activemodel.rb
@@ -1,4 +1,40 @@
 require 'active_model'
+module GollumRails
+  module Adapters
+    
+    # ActiveModel improvements and own connectors
+    #
+    # including:
+    #   * validation
+    #   * callbacks
+    #   * naming
+    #   * error handling
+    # 
+    # The following files are involved:
+    #   * boolean.rb -> Boolean features for validation
+    #   * callback.rb -> Callback functionality
+    #   * naming.rb -> Conversion and Naming
+    #   * error.rb -> Active Model error handling
+    #   * validation.rb -> Object validation
+    #
+    # Released under the AGPL License. For further information see the LICENSE file distributed
+    # with this package.
+    #
+    # TODO:
+    #   * a lot of testing
+    #   * implementing the rest of the error class
+    #   * deleting the model template out
+    #   * move into own gem
+    #
+    # FIXME:
+    #   none a.t.m.
+    #
+    module ActiveModel
+      VERSION="0.0.4"
+    end
+  end
+end
+
 require File.expand_path '../activemodel/boolean', __FILE__
 require File.expand_path '../activemodel/callback', __FILE__
 require File.expand_path '../activemodel/naming', __FILE__
--- lib/gollum_rails/adapters/activemodel/boolean.rb
+++ lib/gollum_rails/adapters/activemodel/boolean.rb
@@ -1,8 +1,14 @@
 module GollumRails
   module Adapters
     module ActiveModel
+
+      # Own implemented Boolean method for validating
       module Boolean; end
+
+      # inherit
       class ::TrueClass; include Boolean; end
+
+      # inherit
       class ::FalseClass; include Boolean; end
     end
   end
