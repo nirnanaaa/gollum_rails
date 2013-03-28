ee3cafec626acd674f131b49f8b3e89f70f469fe
nirnanaaa <mosny@zyg.li>
2013-03-28 21:59:04 +0100

    added wiki creation

--- lib/gollum_rails/adapters/gollum/wiki.rb
+++ lib/gollum_rails/adapters/gollum/wiki.rb
@@ -1,12 +1,31 @@
+require 'grit'
 module GollumRails
   module Adapters
     module Gollum
 
       # TODO: doc
-      class  Wiki < ::Gollum::Wiki
-        def initialize(location)
+      class  Wiki
 
+        # Gets / Sets the git path or object
+        attr_accessor :git
+        
+        # Gets / Sets the wiki instance
+        attr_accessor :wiki
+
+        def initialize(location)
+          gollum = ::Gollum::Wiki
+          @git = location
+          if location.is_a? ::String
+            @wiki = gollum.new @git
+          else
+            @wiki = gollum.new @git.path
+          end
         end
+
+        def self.wiki(location)
+          Wiki.new(location)
+        end
+
       end
     end
   end
--- spec/gollum_rails/adapters/gollum/wiki_spec.rb
+++ spec/gollum_rails/adapters/gollum/wiki_spec.rb
@@ -3,12 +3,21 @@ require 'grit'
 
 describe GollumRails::Adapters::Gollum::Wiki do
   before(:each) do
-    location = "#{File.dirname(__FILE__)}/../../../utils/wiki"
-    puts location
-    @repo = Grit::Repo.init_bare location
-    @wiki = GollumRails::Adapters::Gollum::Wiki.new(location)
+    @location = "#{File.dirname(__FILE__)}/../../../utils/wiki.git"
+    @repo = Grit::Repo.init_bare @location
+    @wiki = GollumRails::Adapters::Gollum::Wiki.new @repo
   end
-  it "should initialize a new wiki instance" do
-    
+  it "should test initializer" do
+    wiki = GollumRails::Adapters::Gollum::Wiki.new @location
+    @wiki.git.should == @repo
+    wiki.git.should be_instance_of(String)
+    wiki.git.should == @location
   end
+  it "should test the creation of a new wiki" do
+    wiki = GollumRails::Adapters::Gollum::Wiki.wiki @repo
+    wiki.git.should == @repo
+    wiki = GollumRails::Adapters::Gollum::Wiki.wiki @location
+    wiki.git.should == @location
+  end
+
 end
