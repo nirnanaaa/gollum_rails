1edddfb7b0cfa9a0063a0f5f2a359f88c24f838e
Florian Kasper <florian.kasper@corscience.de>
2013-03-28 13:19:39 +0100

    callback  content

--- lib/gollum_rails/adapters/activemodel/error.rb
+++ lib/gollum_rails/adapters/activemodel/error.rb
@@ -1,18 +1,26 @@
 module GollumRails
   module Adapters
     module ActiveModel
+
+      # Error handling class, with several exception types and debug / info messages
       class Error < ::GollumRails::GollumInternalError
         extend ::ActiveModel::Naming
         
+        #######
         private
-        
+        #######
+
+        # Gets the Priority
         attr_reader :priority
 
+        ######
         public
+        ######
+
         # Gets/Sets the Error
         attr_reader :errors
 
-        # Initializes a new Instance of ActiveModel
+        # Initializes a new Exception
         # 
         def initialize(name, message = nil, stack = nil, priority = :crit)
           if priority == :high or priority == :crit
