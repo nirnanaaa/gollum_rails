3338c225d31d39cfa62302c0fa154ef3d4ef8e07
nirnanaaa <mosny@zyg.li>
2013-03-28 11:36:29 +0100

    updated docblock

--- lib/gollum_rails/adapters/activemodel/error.rb
+++ lib/gollum_rails/adapters/activemodel/error.rb
@@ -2,16 +2,29 @@ module GollumRails
   module Adapters
     module ActiveModel
       class Error < ::GollumRails::GollumInternalError
-        extend Naming::
+        extend ::ActiveModel::Naming
         
+        private
+        
+        attr_reader :priority
+
+        public
         # Gets/Sets the Error
-        attr_accessor :errors
+        attr_reader :errors
 
         # Initializes a new Instance of ActiveModel
         # 
-        def initialize
+        def initialize(name, message = "", stack = nil, priority = :low)
           @errors = ActiveModel::Errors.new(self)
+          if priority == :high or priority == :crit
+            raise self, "Error thrown: #{name},\n #{(stack || message)}"
+          else
+            errors.add priority.to_sym
+          end
+
         end
+
+
       end
 
     end
