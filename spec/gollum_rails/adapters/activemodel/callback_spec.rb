b1f59de08d0d517fa6eadfb41bf6b8cdde26a3fc
Florian Kasper <florian.kasper@corscience.de>
2013-03-28 13:50:33 +0100

    added specs

---
+++ spec/gollum_rails/adapters/activemodel/callback_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe GollumRails::Adapters::ActiveModel::Callback do
+  it "s" do
+    false.should be_true
+
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,18 @@ end
 
 RSpec.configure do |config|
  config.treat_symbols_as_metadata_keys_with_true_values = true
+
+ config.backtrace_clean_patterns = [
+  /\/lib\d*\/ruby\//,
+  /bin\//,
+  /gems/,
+  /spec\/spec_helper\.rb/,
+  /lib\/rspec\/(core|expectations|matchers|mocks)/
+ ]
+ config.full_backtrace= false # save the console
+ config.color_enabled= true   # save your eyes
+ config.formatter = :documentation
+
  config.mock_with :rr
 
  config.before(:each) do
