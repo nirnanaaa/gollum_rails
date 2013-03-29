7a9b3b261eb878c39139c7d2648f05b70827f87f
nirnanaaa <mosny@zyg.li>
2013-03-29 08:45:34 +0100

    added initializer and setup

--- lib/gollum_rails/adapters/gollum/wiki.rb
+++ lib/gollum_rails/adapters/gollum/wiki.rb
@@ -8,20 +8,31 @@ module GollumRails
 
         # Gets / Sets the git path or object
         attr_accessor :git
-        
-        # Gets / Sets the wiki instance
-        attr_accessor :wiki
 
+        # Static callers
+        class << self
+          # Gets / Sets the wiki instance
+          attr_accessor :wiki
+        end
+
+        # Initializes the class
+        #
+        # location - String or Grit::Repo
         def initialize(location)
           gollum = ::Gollum::Wiki
           @git = location
           if location.is_a? ::String
-            @wiki = gollum.new @git
+            @@wiki = gollum.new @git
           else
-            @wiki = gollum.new @git.path
+            @@wiki = gollum.new @git.path
           end
         end
+      
+        
 
+        # Static call from within any other class
+        #
+        # Returns a new instance of this class
         def self.wiki(location)
           Wiki.new(location)
         end
