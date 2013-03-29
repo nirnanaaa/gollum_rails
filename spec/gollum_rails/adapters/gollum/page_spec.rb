91a898f0c4ac43de75159e9e2c326922dad1fdd2
nirnanaaa <mosny@zyg.li>
2013-03-29 10:38:59 +0100

    delete working

--- spec/gollum_rails/adapters/gollum/page_spec.rb
+++ spec/gollum_rails/adapters/gollum/page_spec.rb
@@ -6,6 +6,11 @@ describe GollumRails::Adapters::Gollum::Page do
       :name => 'The Mosny',
       :email => 'mosny@zyg.li'
     }
+
+    @location = "#{File.dirname(__FILE__)}/../../../utils/wiki.git"
+    @repo = Grit::Repo.init_bare @location
+    @wiki = GollumRails::Adapters::Gollum::Wiki.new @repo
+    @page = GollumRails::Adapters::Gollum::Page.new 
   end
   it "should initialize the correct wiki" do
     location = "#{File.dirname(__FILE__)}/../../../utils/wiki.git"
@@ -19,7 +24,18 @@ describe GollumRails::Adapters::Gollum::Page do
     repo = Grit::Repo.init_bare location
     wiki = GollumRails::Adapters::Gollum::Wiki.new repo
     page = GollumRails::Adapters::Gollum::Page.new 
-    page.new_page("testpage", "content",:markdown, @commit )
+    page.new_page("testpage", "content",:markdown, @commit ).should be_instance_of Gollum::Page
     page.delete_page(nil, @commit)
+  end
+
+  it "should delete an existing page" do
+    page = @page.new_page 'testpage', 'content', :markdown, @commit
+    @page.delete_page(page, @commit).should be_instance_of String
+    
+    @page.new_page 'testpage', 'content', :markdown, @commit
+    @page.delete_page(nil, @commit).should be_instance_of String
+    
+  end
+  it "should update an existing page" do 
   end
 end
