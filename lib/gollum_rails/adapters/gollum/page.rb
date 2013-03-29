0be65819aef1efa71e832bc369c97d3c76859695
nirnanaaa <mosny@zyg.li>
2013-03-29 09:10:32 +0100

    updated page page

--- lib/gollum_rails/adapters/gollum/page.rb
+++ lib/gollum_rails/adapters/gollum/page.rb
@@ -4,6 +4,52 @@ module GollumRails
 
       # TODO: doc
       class Page
+        
+        # Gets / Sets current page
+        attr_accessor :page
+
+        # Gets / Sets the wiki
+        attr_accessor :wiki
+
+        # Initializer
+        def initialize
+          wiki = GollumRails::Adapters::Gollum::Wiki.class_variable_get(:@@wiki)
+          if wiki.is_a? ::Gollum::Wiki
+            @wiki = wiki
+          else
+            Error.new('initialized wiki is not an instance of Gollum::Wiki')
+          end
+        end
+
+
+        # creates a new Page
+        #
+        # Returns the commit id
+        def new_page()
+        end
+
+        # updates an existing page
+        #
+        # Returns the commit id
+        def update_page()
+        end
+
+        # deletes an existing page
+        #
+        # Returns the commit id
+        def delete_page()
+        end
+        
+        # renames an existing page
+        #
+        # Returns the commit id
+        def rename_page()
+        end
+        
+        # moves an existing page
+        def move_page()
+        end
+
       end
     end
   end
