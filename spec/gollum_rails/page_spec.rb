f4efc5e601ea5606fa0ac94f34e1a6e27635fbb9
nirnanaaa <mosny@zyg.li>
2013-04-01 16:58:12 +0200

    specs updated

--- spec/gollum_rails/page_spec.rb
+++ spec/gollum_rails/page_spec.rb
@@ -1,146 +1,158 @@
 require 'spec_helper'
 
-describe GollumRails::Page do
+describe "Gollum Page" do
   before(:each) do
-    @commit = {
-      :name => "flo",
-      :message => "commit",
-      :email => "mosny@zyg.li"
-    }
-    @call = {
-      :name => "Goole", 
-      :content => "content data", 
-      :commit => @commit, 
-      :format => :markdown
-    }
-  end
-
-  class RailsModel < GollumRails::Page
-
-    register_validations_for :name,
-                             :format
-
-    validate do |validator|
-      validator.test(@format, "type=Object")
+      @commit = {
+        :name => "flo",
+        :message => "commit",
+        :email => "mosny@zyg.li"
+      }
+      @call = {
+        :name => "Goole", 
+        :content => "content data", 
+        :commit => @commit, 
+        :format => :markdown
+      }
     end
-  end
-  it "should test the creation of a page" do
-    rr = RailsModel.new(@call)
-    rr.save.should be_instance_of Gollum::Page
-    rr.save!.should be_instance_of Gollum::Page
-    RailsModel.create(@call)
-  end
-  
-  it "should test the update of a page" do
-    rr = RailsModel.new @call
-    cc = rr.save.should be_instance_of Gollum::Page
-    rr.update_attributes({:name => "google", :format => :wiki}).should be_instance_of Gollum::Page
-  end
+  describe GollumRails::Page do
+    class RailsModel < GollumRails::Page
 
-  it "should test the deletion of a page" do
-    rr = RailsModel.new @call
-    cc = rr.save.should be_instance_of Gollum::Page
-    rr.delete.should be_instance_of String
-  end
+      register_validations_for :name,
+                               :format
 
-  it "should test the finding of a page" do
-    RailsModel.find('google').should be_instance_of Gollum::Page
-
-    #invalid input
-    RailsModel.find('<script type="text/javascript">alert(123);</script>').should be_nil
-  end
-  it "should test the preview" do
-    rr = RailsModel.new :content => "# content", :name => "somepage"
-    100.times do
-      rr.preview.should include("<h1>content<a class=\"anchor\" id=\"content\" href=\"#content\"></a></h1>")
+      validate do |validator|
+        validator.test(@format, "type=Object")
+      end
     end
-  end
+    it "should test the creation of a page" do
+      rr = RailsModel.new(@call)
+      rr.save.should be_instance_of Gollum::Page
+      rr.save!.should be_instance_of Gollum::Page
+      RailsModel.create(@call)
+    end
+    
+    it "should test the update of a page" do
+      rr = RailsModel.new @call
+      cc = rr.save.should be_instance_of Gollum::Page
+      rr.update_attributes({:name => "google", :format => :wiki}).should be_instance_of Gollum::Page
+    end
 
-  it "should test exception methods" do
-    create = RailsModel.create! @call
-  end
+    it "should test the deletion of a page" do
+      rr = RailsModel.new @call
+      cc = rr.save.should be_instance_of Gollum::Page
+      rr.delete.should be_instance_of String
+    end
 
-  it "should test the supported formats" do
-    RailsModel.format_supported?('ascii').should be_true
-    RailsModel.format_supported?('markdown').should be_true
-    RailsModel.format_supported?('github-markdown').should be_true
-    RailsModel.format_supported?('rdoc').should be_true
-    RailsModel.format_supported?('org').should be_true
-    RailsModel.format_supported?('pod').should be_true
-  end
+    it "should test the finding of a page" do
+      RailsModel.find('google').should be_instance_of Gollum::Page
 
-  it "should test getters" do
-    rr = RailsModel.new @call
-    rr.name.should == "Goole"
-    rr.content.should == "content data"
-    rr.commit.should be_instance_of Hash
-    rr.commit.should == @commit
-    rr.format.should == :markdown
-    rr.wiki.should be_instance_of Gollum::Wiki
-    rr.save
-    rr.page.should be_instance_of GollumRails::Adapters::Gollum::Page
-  end
-  it "should test setters" do
-    rr = RailsModel.new
-    rr.name=("google").should == "google"
-    rr.commit=(@commit).should == @commit
-    rr.content=("content").should == "content"
-    rr.format=(:markdown).should == :markdown
-  end
-
-  it "should test the static page" do
-    RailsModel.page.should == GollumRails::Adapters::Gollum::Connector.page_class
-  end
-
-  it "should test find or initialize" do
-    rr = RailsModel.new @call
-    rr.save
-    RailsModel.find_or_initialize_by_name(@call[:name], @commit).should  be_instance_of Gollum::Page
-  end
-  it "should test the callback functions" do
-
-    class SaveCallback
-      def self.before_save( obj )
-        obj.name.should == "Goole"
+      #invalid input
+      RailsModel.find('<script type="text/javascript">alert(123);</script>').should be_nil
+    end
+    it "should test the preview" do
+      rr = RailsModel.new :content => "# content", :name => "somepage"
+      100.times do
+        rr.preview.should include("<h1>content<a class=\"anchor\" id=\"content\" href=\"#content\"></a></h1>")
       end
     end
 
-
-    class CallbackTest < GollumRails::Page
-
-      before_save ::SaveCallback
-      after_save :after_save
-      after_delete :after_delete
-      before_delete :before_delete
-      before_update :before_update
-      after_update :after_update
-
-      def after_save
-        @name.should == "Goole"
-      end
-      def before_update
-        @name.should == "Goole" 
-      end
-
-      def after_update
-        @name.should == "Goole"
-      end
-
-      def before_delete
-        @name.should == "Goole" 
-      end
-
-      def after_delete
-        @name.should == "Goole"
-      end
-      
+    it "should test exception methods" do
+      create = RailsModel.create! @call
     end
 
-    test = CallbackTest.new @call
-    test.save
-    test.delete @commit
-    test.save
-    test.update_attributes @call
+    it "should test the supported formats" do
+      RailsModel.format_supported?('ascii').should be_true
+      RailsModel.format_supported?('markdown').should be_true
+      RailsModel.format_supported?('github-markdown').should be_true
+      RailsModel.format_supported?('rdoc').should be_true
+      RailsModel.format_supported?('org').should be_true
+      RailsModel.format_supported?('pod').should be_true
+    end
+
+    it "should test getters" do
+      rr = RailsModel.new @call
+      rr.name.should == "Goole"
+      rr.content.should == "content data"
+      rr.commit.should be_instance_of Hash
+      rr.commit.should == @commit
+      rr.format.should == :markdown
+      rr.wiki.should be_instance_of Gollum::Wiki
+      rr.save
+      rr.page.should be_instance_of GollumRails::Adapters::Gollum::Page
+    end
+    it "should test setters" do
+      rr = RailsModel.new
+      rr.name=("google").should == "google"
+      rr.commit=(@commit).should == @commit
+      rr.content=("content").should == "content"
+      rr.format=(:markdown).should == :markdown
+    end
+
+    it "should test the static page" do
+      RailsModel.page.should == GollumRails::Adapters::Gollum::Connector.page_class
+    end
+
+    it "should test find or initialize" do
+      rr = RailsModel.new @call
+      rr.save
+      RailsModel.find_or_initialize_by_name(@call[:name], @commit).should  be_instance_of Gollum::Page
+    end
+  end
+  describe "callbacks" do
+
+
+
+    it "should test the callback functions" do
+
+      class SaveCallback
+        def self.before_save( obj )
+          obj.name.should == "Goole"
+        end
+      end
+
+
+      class CallbackTest < GollumRails::Page
+
+        before_save ::SaveCallback
+        after_save :after_save
+        after_delete :after_delete
+        before_delete :before_delete
+        before_update :before_update
+        after_update :after_update
+
+        def after_save
+          @name.should == "Goole"
+        end
+        def before_update
+          @name.should == "Goole" 
+        end
+
+        def after_update
+          @name.should == "Goole"
+        end
+
+        def before_delete
+          @name.should == "Goole" 
+        end
+
+        def after_delete
+          @name.should == "Goole"
+        end
+        
+      end
+
+      test = CallbackTest.new @call
+      test.save
+      test.delete @commit
+      test.save
+      test.update_attributes @call
+    end
+  end
+  describe "rails extension" do
+    it "should test 100 saves" do
+
+    end
+
   end
 
 end
