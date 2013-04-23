35bd919316fdda34511c4de55580b64bef15f3b6
nirnanaaa <mosny@zyg.li>
2013-04-23 20:00:57 +0200

    added core patches for compability with ruby 2.0

--- lib/gollum_rails.rb
+++ lib/gollum_rails.rb
@@ -1,6 +1,7 @@
+require 'grit/git-ruby/internal/pack'
 require 'rubygems'
 require 'gollum-lib'
-
+require 'core_ext/string'
 # GollumRails is a RubyGem for extending Rails and the Gollum wiki powered by github
 # It has the ability to combine the benefits from a git powered wiki with Rails.
 #
@@ -12,17 +13,17 @@ require 'gollum-lib'
 module GollumRails
   autoload :Page,     'gollum_rails/page'
   autoload :Setup,    'gollum_rails/setup'
-  
+
   # GollumRails version string
   VERSION = '1.0.1'
-  
+
   # Simplified error
   class Error < StandardError; end
 
   # For use with internal gollumGem exceptions
-  # 
+  #
   class GollumInternalError < Error
-  
+
   end
 end
 
@@ -33,5 +34,3 @@ require File.expand_path '../gollum_rails/adapters/gollum', __FILE__
 #require File.expand_path '../gollum_rails/page', __FILE__
 # load extensions
 require File.expand_path '../gollum_rails/modules/loader', __FILE__
-
-
