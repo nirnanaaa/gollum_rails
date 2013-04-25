108c90ca5129a6e96b29afca18d2e1db756ec435
nirnanaaa <mosny@zyg.li>
2013-04-25 19:18:10 +0200

    bumped version

--- lib/gollum_rails.rb
+++ lib/gollum_rails.rb
@@ -16,7 +16,7 @@ module GollumRails
   autoload :Setup,    'gollum_rails/setup'
 
   # GollumRails version string
-  VERSION = '1.0.2'
+  VERSION = '1.0.3'
 
   # Simplified error
   class Error < StandardError; end
