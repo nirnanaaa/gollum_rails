9c732133b3dacd5bab26582c270e426d181f7f3f
nirnanaaa <mosny@zyg.li>
2013-04-26 19:37:37 +0200

    removed unused require statements

--- lib/gollum_rails/adapters/gollum.rb
+++ lib/gollum_rails/adapters/gollum.rb
@@ -66,9 +66,3 @@ module GollumRails
     end
   end
 end
-
-#require File.expand_path '../gollum/error', __FILE__
-#require File.expand_path '../gollum/wiki', __FILE__
-#require File.expand_path '../gollum/committer', __FILE__
-#require File.expand_path '../gollum/page', __FILE__
-
