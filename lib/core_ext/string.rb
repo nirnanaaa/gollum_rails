af82d74d1651b2d7444ef24d82d322b58f232d32
nirnanaaa <mosny@zyg.li>
2013-05-25 20:11:29 +0200

    added ruby 2.1 support

--- lib/core_ext/string.rb
+++ lib/core_ext/string.rb
@@ -1,10 +1,7 @@
 class String
-  if ((defined? RUBY_VERSION) && (['1.9', '2.0'].include? RUBY_VERSION[0..2]))
+  if ((defined? RUBY_VERSION) && (['1.9', '2.0', '2.1'].include? RUBY_VERSION[0..2]))
     def getord(offset); self[offset].ord; end
   else
     alias :getord :[]
   end
-
-
-
 end
