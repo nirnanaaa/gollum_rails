ff9efacaed12860e0b55f2bd338e5fca7baaf5d7
nirnanaaa <mosny@zyg.li>
2013-05-25 20:43:20 +0200

    removed unused stuff

--- lib/core_ext/string.rb
+++ lib/core_ext/string.rb
@@ -1,7 +1,3 @@
 class String
-  if ((defined? RUBY_VERSION) && (['1.9', '2.0', '2.1', '2.2'].include? RUBY_VERSION[0..2]))
     def getord(offset); self[offset].ord; end
-  else
-    alias :getord :[]
-  end
 end
