16d6fe9b8795a838bdf67f917cec94452ee6acaa
nirnanaaa <mosny@zyg.li>
2013-05-25 20:42:53 +0200

    musn't add specs for string (loading)

--- lib/core_ext/string.rb
+++ lib/core_ext/string.rb
@@ -1,3 +1,7 @@
 class String
+  if ((defined? RUBY_VERSION) && (['1.9', '2.0', '2.1', '2.2'].include? RUBY_VERSION[0..2]))
     def getord(offset); self[offset].ord; end
+  else
+    alias :getord :[]
+  end
 end
