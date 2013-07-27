59d17bd97a4ca003c8ed528f1d71476618e4d8c5
nirnanaaa <mosny@zyg.li>
2013-07-27 09:27:38 +0200

    removed CR

--- spec/gollum_rails/adapters/gollum/page_spec.rb
+++ spec/gollum_rails/adapters/gollum/page_spec.rb
@@ -12,6 +12,18 @@ describe GollumRails::Adapters::Gollum::Page do
       adapter = GollumRails::Adapters::Gollum::Page.new
       GollumRails::Adapters::Gollum::Connector.page_class.should == GollumRails::Adapters::Gollum::Page
     end
-    
+  end
+  describe "statically methods" do
+    it "is a hash" do
+      GollumRails::Adapters::Gollum::Page.parse_path('google/de').should be_a Hash
+    end
+    it "should equal" do
+      path1 = GollumRails::Adapters::Gollum::Page.parse_path('google/de')
+      path2 = GollumRails::Adapters::Gollum::Page.parse_path('/google/de')
+      path1.should == path2
+    end
+    it "adds a leading slash" do
+      GollumRails::Adapters::Gollum::Page.parse_path('google/de')[:path][0].should == '/'
+    end
   end
 end
