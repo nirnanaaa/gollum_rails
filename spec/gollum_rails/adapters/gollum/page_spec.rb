66cea555de632aa6712b569fc9bc7e9178bd6b51
nirnanaaa <mosny@zyg.li>
2013-07-27 09:23:01 +0200

    removed obsolete accessors

--- spec/gollum_rails/adapters/gollum/page_spec.rb
+++ spec/gollum_rails/adapters/gollum/page_spec.rb
@@ -7,4 +7,11 @@ describe GollumRails::Adapters::Gollum::Page do
       :email => 'mosny@zyg.li'
     }
   end
+  describe "Connector stuff" do
+    it "should have updated the page_class on initialize" do
+      adapter = GollumRails::Adapters::Gollum::Page.new
+      GollumRails::Adapters::Gollum::Connector.page_class.should == GollumRails::Adapters::Gollum::Page
+    end
+    
+  end
 end
