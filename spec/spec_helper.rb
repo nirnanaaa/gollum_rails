52d06fdf801bb73e2d03ccec947fcdc459390e5f
nirnanaaa <mosny@zyg.li>
2013-08-23 07:11:11 +0200

    using dots instead of that massive output

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,33 +7,13 @@ Coveralls.wear!
 require 'gollum_rails'
 
 
-require 'benchmark'
-RSpec::Matchers.define :take_less_than do |n|
-  chain :seconds do; end
-    match do |block|
-      @elapsed = Benchmark.realtime do
-        block.call
-      end
-      @elapsed <= n
-    end
-end
 
 RSpec.configure do |config|
  config.treat_symbols_as_metadata_keys_with_true_values = true
 
- config.backtrace_clean_patterns = [
-  /\/lib\d*\/ruby\//,
-  /bin\//,
-  /gems/,
-  /spec\/spec_helper\.rb/,
-  /lib\/rspec\/(core|expectations|matchers|mocks)/
- ]
  config.before(:each) do
    GollumRails::Adapters::Gollum::Connector.enabled = true
  end
- config.full_backtrace= false # save the console
- config.color_enabled= true   # save your eyes
- config.formatter = :documentation
 
  config.mock_with :rr
 
