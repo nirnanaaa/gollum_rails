25e247a114888d82e57beecd9d83da4f2b5ce728
nirnanaaa <mosny@zyg.li>
2014-01-13 09:30:27 +0100

    removed initialization

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,9 +11,6 @@ require 'gollum_rails'
 RSpec.configure do |config|
  config.treat_symbols_as_metadata_keys_with_true_values = true
 
- config.before(:each) do
-   GollumRails::Adapters::Gollum::Connector.enabled = true
- end
 
  config.mock_with :rr
  
